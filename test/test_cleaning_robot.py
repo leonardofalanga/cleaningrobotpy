31f89a3906d907f91cbf9c7e9d8dfc82a0ce7be4
Leonardo Falanga <info@leonardofalanga.it>
2024-12-04 16:57:41 +0100

    Red phase user story 1

--- test/test_cleaning_robot.py
+++ test/test_cleaning_robot.py
@@ -8,9 +8,11 @@ from src.cleaning_robot import CleaningRobot
 
 class TestCleaningRobot(TestCase):
 
-    @patch.object(GPIO, "input")
-    def test_something(self, mock_object: Mock):
-        # This is an example of test where I want to mock the GPIO.input() function
-        pass
+    def initialize_robot(self):
+        system=CleaningRobot()
+        system.initialize_robot()
+        self.assertEquals(system.robot_status(),"(0,0,N)")
+
+
 
 
