66c084705fd89b112159cbdd39ccc7c33eddd9f2
Leonardo Falanga <info@leonardofalanga.it>
2024-12-04 17:50:01 +0100

    Red phase user story 2

--- test/test_cleaning_robot.py
+++ test/test_cleaning_robot.py
@@ -13,6 +13,23 @@ class TestCleaningRobot(TestCase):
         system.initialize_robot()
         self.assertEqual(system.robot_status(),"(0,0,N)")
 
+    @patch.object(IBS, "get_charge_left")
+    @patch.object(GPIO, "output")
+    def test_manage_cleaning_system_up_10(self, mock_ledclean: Mock, mock_ibs: Mock):
+        system=CleaningRobot()
+        mock_ibs.return_value= 15
+        system.manage_cleaning_system()
+        mock_ledclean.assert_has_calls([call(system.RECHARGE_LED_PIN, GPIO.LOW), call(system.CLEANING_SYSTEM_PIN, GPIO.HIGH)])
+
+    @patch.object(IBS, "get_charge_left")
+    @patch.object(GPIO, "output")
+    def test_manage_cleaning_system_minus_10(self, mock_ledclean: Mock, mock_ibs: Mock):
+        system = CleaningRobot()
+        mock_ibs.return_value= 5
+        system.manage_cleaning_system()
+        mock_ledclean.assert_has_calls([call(system.RECHARGE_LED_PIN, GPIO.HIGH), call(system.CLEANING_SYSTEM_PIN, GPIO.LOW)])
+
+
 
 
 
